76ea18ac9c550782ef1ff9584db2a624fee7790f
sorahashiroi <sorachansiroi124@gmail.com>
2025-04-25 11:33:41 +0900

    The initial programs

---
+++ cmd/main/difson_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func Example_difson() {
+	goMain([]string{"difson"})
+}
+
+func TestHello(t *testing.T) {
+	got := hello()
+	want := "Welcome to Difson!"
+	if got != want {
+		t.Errorf("hello() = %q, want %q", got, want)
+	}
+}
