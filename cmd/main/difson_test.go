e66639817f41694a2a895d9f699648531f371579
sorahashiroi <sorachansiroi124@gmail.com>
2025-05-08 17:29:02 +0900

    update gofiles

--- cmd/main/difson_test.go
+++ cmd/main/difson_test.go
@@ -3,15 +3,15 @@ package main
 import "testing"
 
 func Example_difson() {
-		goMain([]string{"difson"})
-		// Output:
-		// Welcome to Difson!
+	goMain([]string{"difson"})
+	// Output:
+	// Welcome to Difson!
 }
 
 func TestHello(t *testing.T) {
-		got := hello()
-		want := "Welcome to Difson!"
-		if got != want {
-				t.Errorf("hello() = %q, want %q", got, want)
-		}
+	got := hello()
+	want := "Welcome to Difson!"
+	if got != want {
+			t.Errorf("hello() = %q, want %q", got, want)
+	}
 }
